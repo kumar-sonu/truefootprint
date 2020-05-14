bfff01f29411d0fdc6b080a1ad3091f6cff06805
Sonu Kumar <railssonukumar@gmail.com>
2020-05-14 18:12:15 +0530

    Add Message controller specs

---
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::MessagesController do
+  describe 'GET #index' do
+    let!(:message) { FactoryBot.create(:message) }
+    before do
+      get :index
+    end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'JSON body response contains expected message attributes' do
+      json_response = JSON.parse(response.body)
+      expect(json_response['results'].first.keys).to match_array(['id', 'body', 'sender', 'posted_at'])
+    end
+  end
+
+  describe 'POST #create' do
+    context 'send post request' do
+      let(:params) {
+        {
+          body: 'This is a test post',
+          sender: 'Robert',
+          posted_at: Time.now
+        }
+      }
+      before do
+        post :create, params: params
+      end
+      it { expect(Message.count).to eq(1) }
+      it { expect(Message.first.body).to eq('This is a test post') }
+      it { expect(Message.first.sender).to eq('Robert') }
+      it { expect(response).to have_http_status(:success)  }
+    end
+  end
+end
